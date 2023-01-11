67534b6e96de80c9b5067f9317a5fa4000f73188
Tootin0730 <yisan1004@gmail.com>
2023-01-11 15:07:21 -0500

    3.8 Servers recap
    There were no changes in codes in 3.7 Setup recap

--- src/server.js
+++ src/server.js
@@ -4,16 +4,9 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
+const handleHome = () => console.log("home");
 
-const handleHome = (req, res) => {
-    return res.send("I love middlewares");
-};
-
-app.get("/", logger, handleHome);
+app.get("/", handleHome);
 
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
