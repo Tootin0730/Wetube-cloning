bbb278460264173da4de32370788b06e15b155bd
Tootin0730 <yisan1004@gmail.com>
2023-01-12 12:38:22 -0500

    3.10 Middleware recap

--- src/server.js
+++ src/server.js
@@ -4,8 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
+const routerLogger = (req, res, next) => {
+    next();
+};
+const methodLogger = (req, res, next) =>{
+    next();
+};
+
 const home = (req, res) => res.send("hello");
-app.get("/", home);
+app.get("/", methodLogger, routerLogger, home);
 
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
