daded041c17a247e05718b776f716bbba4aff7d3
Tootin0730 <yisan1004@gmail.com>
2023-01-12 12:50:33 -0500

    3.11 External Middleware
    using morgan

--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 import express from "express";
+import logger from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const routerLogger = (req, res, next) => {
-    next();
-};
-const methodLogger = (req, res, next) =>{
-    next();
+const home = (req, res) => {
+    return res.send("hello");
+}
+
+const login = (req, res) => {
+    return res.send("login");
 };
 
-const home = (req, res) => res.send("hello");
-app.get("/", methodLogger, routerLogger, home);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
