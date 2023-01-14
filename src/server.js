68c51b7652d299c8c5e15a49621c637848731b5a
Tootin0730 <yisan1004@gmail.com>
2023-01-14 11:32:05 -0500

    4.1 Making our Router (fixed)
    fixed minor errors from the previous codes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import logger from "morgan";
+import morgan from "morgan";
 
 const PORT = 4000;
 
@@ -13,7 +13,7 @@ const handleHome = (req, res) => res.send("Home");
 
 globalRouter.get("/", handleHome);
 
-const userRouter = express.send();
+const userRouter = express.Router();
 
 const handleEditUser = (req, res) => res.send("Edit User");
 
@@ -25,9 +25,9 @@ const handleWatchVideo = (req, res) => res.send("Watch Video");
 
 videoRouter.get("/watch", handleWatchVideo);
 
-app.get("/", globalRouter);
-app.get("/videos", videoRouter);
-app.get("/users", useroRouter);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
