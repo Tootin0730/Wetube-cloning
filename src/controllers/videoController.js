d2238c80c06544586c991cb8f375b4e66d791c92
Tootin0730 <yisan1004@gmail.com>
2023-01-16 17:38:22 -0500

    5.0 Returning HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,16 @@
-export const trending = (req, res) => res.send("Home Page videos");
+export const trending = (req, res) => 
+    res.send(
+      "<!DOCTYPE html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy; 2023 Wetube - All rights reserved </footer></body></html>"
+    );
 export const see = (req, res) => {
-    return res.send(`Watch video #${req.params.id}`);
+    return res.send(
+        `<!DOCTYPE html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id} </h1><footer>&copy; 2023 Wetube - All rights reserved </footer></body></html>`
+    );
 };
 export const edit = (req, res) => {
-    return res.send("Edit");
+    return res.send(
+        `<!DOCTYPE html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id} </h1><footer>&copy; 2023 Wetube - All rights reserved </footer></body></html>`
+    );
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
