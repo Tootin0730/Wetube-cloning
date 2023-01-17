469c3b779f340539d4baf2e8fed78d464ede7008
Tootin0730 <yisan1004@gmail.com>
2023-01-17 12:26:51 -0500

    5.1 Configuration pug
    source must be fixed

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
+console.log(process.cwd())
+
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
