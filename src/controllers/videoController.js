b1cf17ac0471a3a5b74969bf616d22a3d10d0fac
Tootin0730 <yisan1004@gmail.com>
2023-01-22 10:27:11 -0500

    6.0 array Database pt.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,4 @@
-export const trending = (req, res) => {
-    const videos = [
+let videos = [
     {
         title: "First video",
         rating: 5,
@@ -14,23 +13,27 @@ export const trending = (req, res) => {
         comments: 2,
         createdAd: "2 minutes ago",
         views: 59,
-        id: 1,
+        id: 2,
     },
     {
-        title: "Thirs video",
+        title: "Third video",
         rating: 5,
         comments: 2,
         createdAd: "2 minutes ago",
         views: 59,
-        id: 1,
+        id: 3,
     },
 ];
+
+export const trending = (req, res) => {
     return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    return res.render("watch", {pageTitle: `Watch ${video.title}` });
+}
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-};;
+export const deleteVideo = (req, res) => res.send("Delete Video");
