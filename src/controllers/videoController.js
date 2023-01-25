152d9e6be9975268ab9d44d5e15f659d3dfa7973
Tootin0730 <yisan1004@gmail.com>
2023-01-25 10:31:54 -0500

    6.5 More practice pt.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,17 +30,26 @@ export const trending = (req, res) => {
 };
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
+    const video = videos[id - 1];
     return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 }
-export const edit = (req, res) => {
+export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("edit", { pageTitle: `Editing: ${video.title}` } );
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video } );
 };
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
     videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+    //here we will add a video to the videos array.
+    return res.redirect("/");
 };
